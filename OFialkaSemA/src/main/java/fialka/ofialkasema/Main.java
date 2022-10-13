2b1479d7024402ee46ce535b60094a7dc75dab2e
Fialka Ondrej <st64116@upce.cz>
2022-10-13 16:56:47 +0200

    sema

--- OFialkaSemA/src/main/java/fialka/ofialkasema/Main.java
+++ OFialkaSemA/src/main/java/fialka/ofialkasema/Main.java
@@ -19,21 +19,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         int pocetSimulaci = 0;
-        double vsechnyCeny = 0;
-        double prumernaCena = 0;
+        double vsechnyCeny = 0, prumernaCena = 0;
+
+        double smerOdchylka1, smerOdchylka2 = 1000;
 
         LinkedList<Long> cenySimulaci = new LinkedList();
 
-        while (pocetSimulaci < 100000) {
+        do {
+            pocetSimulaci++;
+            smerOdchylka1 = smerOdchylka2;
             Simulace sim = new Simulace();
             sim.spocitejCennyCest();
             long nejlevnejsiCesta = Math.round(sim.getNejlevnejsiCestu());
             vsechnyCeny += nejlevnejsiCesta;
             cenySimulaci.add(nejlevnejsiCesta);
             prumernaCena = vsechnyCeny / pocetSimulaci;
-            pocetSimulaci++;
-        }
+            smerOdchylka2 = spocitejOdchylku(cenySimulaci, prumernaCena);
+            System.out.println(smerOdchylka2);
+        } while (Math.abs(smerOdchylka1 - smerOdchylka2) > 0.00000001);
+//        }while(pocetSimulaci < 100000);
+
         zapisCeny(cenySimulaci);
+        System.out.println(pocetSimulaci);
         System.out.println("prumerna cena projektu: " + Math.round(prumernaCena) * 1000 + " Kč,-");
 
     }
@@ -42,10 +49,9 @@ public class Main {
         File f = new File("./hodnoty.csv");
         FileWriter fw = new FileWriter(f);
         fw.write("Reálný rozpočet projektu [v tisících Kč];pravděpodobnost[%]\n");
-        
-        
+
         for (int i = 17; i < 51; i++) {
-            double procenta = Math.floor((getPercentage(i, list)*100))/100;
+            double procenta = Math.floor((getPercentage(i, list) * 100)) / 100;
             if (procenta > 0.009) {
                 fw.write(i + ";" + String.valueOf(procenta).replace(".", ",") + "\n");
             }
@@ -68,5 +74,15 @@ public class Main {
             }
         }
         return count;
+    }
+
+    private static double spocitejOdchylku(LinkedList<Long> list, double prumer) {
+        double pom = 0;
+        for (long cena : list) {
+            pom += (cena - prumer) * (cena - prumer);
+        }
+        double p = 1.0 / list.size();
+        double rozptyl = (p * pom);
+        return Math.sqrt(rozptyl);
     }
 }
